a5951b4bb7a0798b221299d8e380a35fc5416f14
ukastsiukevich <grun_gespenst@tut.by>
2025-02-15 09:59:03 +0300

    feat: add files

--- memcache_load/memc_load.py
+++ memcache_load/memc_load.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""Memcache Load module."""
+
 import os
 import gzip
 import sys
@@ -13,7 +13,8 @@ from collections import defaultdict
 from memcache import Client
 from optparse import OptionParser
 
-import appsinstalled_pb2
+from appsinstalled_pb2 import UserApps
+
 
 AppsInstalled = collections.namedtuple("AppsInstalled", ["dev_type", "dev_id", "lat", "lon", "apps"])
 
@@ -23,23 +24,25 @@ MAXATTEMPTS = 5
 
 
 class ProcessedAppsCounter(object):
-    """
-    Class Helper to count errors and processed apps
-    """
+    """Class Helper to count errors and processed apps."""
 
     processed_apps = 0
     errors_apps = 0
 
     def error(self, count=1):
+        """Errors."""
         self.errors_apps += count
 
     def processed(self, count=1):
+        """Processed."""
         self.processed_apps += count
 
     def get_processed(self):
+        """Get processed."""
         return self.processed_apps
 
     def get_errors_rate(self):
+        """Get error rate."""
         try:
             err_rate = float(self.errors_apps) / self.processed_apps
         except ZeroDivisionError:
@@ -48,9 +51,7 @@ class ProcessedAppsCounter(object):
 
 
 class Worker(object):
-    """
-    Class used to process files in separate processes
-    """
+    """Class used to process files in separate processes."""
 
     def __init__(self, dry, device_memc):
         self.dry = dry
@@ -63,6 +64,7 @@ class Worker(object):
         self.processing(fn, self.dry)
 
     def processing(self, filename, dry=False):
+        """Process file."""
         processed_counter = ProcessedAppsCounter()
         pool = ThreadPool(self.max_threads)
         device_memc_clients = dict()
@@ -71,7 +73,8 @@ class Worker(object):
         memc_clients_processed_apps = defaultdict(int)
 
         def bufferize_appsinstalled(memc_client, appsinstalled):
-            ua = appsinstalled_pb2.UserApps()
+            """Buffize appsinstalled."""
+            ua = UserApps()
             ua.lat = appsinstalled.lat
             ua.lon = appsinstalled.lon
             key = "%s:%s" % (appsinstalled.dev_type, appsinstalled.dev_id)
@@ -143,12 +146,14 @@ class Worker(object):
 
 
 def dot_rename(path):
+    """Rename file."""
     head, fn = os.path.split(path)
     # atomic in most cases
     os.rename(path, os.path.join(head, "." + fn))
 
 
 def parse_appsinstalled(line):
+    """Parse appsinstalled line."""
     line_parts = line.strip().split("\t")
     if len(line_parts) < 5:
         return
@@ -168,6 +173,7 @@ def parse_appsinstalled(line):
 
 
 def insert_appsinstalled(memc_client, bufferized_dict, error_counter, dry_run=False):
+    """Insert appsinstalled."""
     try:
         if dry_run:
             logging.debug("%s -> %s" % (memc_client, bufferized_dict))
@@ -180,28 +186,29 @@ def insert_appsinstalled(memc_client, bufferized_dict, error_counter, dry_run=Fa
 
 
 def memc_set(client, key_value):
+    """Set memc client."""
     res = None
     for _ in range(MAXATTEMPTS):
         try:
             res = client.set_multi(key_value)
-        except Exception as error:
+        except Exception:
             pass
         if len(res) == 0:
             return True
         time.sleep(TIMEOUT)
     # if we are here then error has occured
-    raise Exception('Max attempts exceeded while trying to connect to {} with error {}'.format(client, error))
+    raise Exception('Max attempts exceeded while trying to connect to {} with error'.format(client))
 
 
 def main(options):
+    """Main function."""
     max_processes = int(options.processes)
     device_memc = {
-            "idfa": options.idfa,
-            "gaid": options.gaid,
-            "adid": options.adid,
-            "dvid": options.dvid,
-        }
-
+        "idfa": options.idfa,
+        "gaid": options.gaid,
+        "adid": options.adid,
+        "dvid": options.dvid,
+    }
 
     process_pool = multiprocessing.Pool(max_processes)
     worker = Worker(options.dry, device_memc)
@@ -209,22 +216,24 @@ def main(options):
 
 
 def prototest():
+    """Prototest."""
     sample = "idfa\t1rfw452y52g2gq4g\t55.55\t42.42\t1423,43,567,3,7,23\ngaid\t7rfw452y52g2gq4g\t55.55\t42.42\t7423,424"
     for line in sample.splitlines():
         dev_type, dev_id, lat, lon, raw_apps = line.strip().split("\t")
         apps = [int(a) for a in raw_apps.split(",") if a.isdigit()]
         lat, lon = float(lat), float(lon)
-        ua = appsinstalled_pb2.UserApps()
+        ua = UserApps()
         ua.lat = lat
         ua.lon = lon
         ua.apps.extend(apps)
         packed = ua.SerializeToString()
-        unpacked = appsinstalled_pb2.UserApps()
+        unpacked = UserApps()
         unpacked.ParseFromString(packed)
         assert ua == unpacked
 
 
 if __name__ == '__main__':
+    """Loader."""
     op = OptionParser()
     op.add_option("-t", "--test", action="store_true", default=False)
     op.add_option("-l", "--log", action="store", default=None)
@@ -236,8 +245,12 @@ if __name__ == '__main__':
     op.add_option("--dvid", action="store", default="127.0.0.1:33016")
     op.add_option("--processes", action="store", default=4)
     (opts, args) = op.parse_args()
-    logging.basicConfig(filename=opts.log, level=logging.INFO if not opts.dry else logging.DEBUG,
-                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+    logging.basicConfig(
+        filename=opts.log,
+        level=logging.INFO if not opts.dry else logging.DEBUG,
+        format='[%(asctime)s] %(levelname).1s %(message)s',
+        datefmt='%Y.%m.%d %H:%M:%S',
+    )
     if opts.test:
         prototest()
         sys.exit(0)
@@ -245,6 +258,6 @@ if __name__ == '__main__':
     logging.info("Memc loader started with options: %s" % opts)
     try:
         main(opts)
-    except Exception, e:
+    except Exception as e:
         logging.exception("Unexpected error: %s" % e)
         sys.exit(1)
